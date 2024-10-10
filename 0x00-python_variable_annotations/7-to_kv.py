6e37698a3d52b2618ca06cf0752834a4f7894457
chika-12 <markworship001@gmail.com>
2024-10-10 22:21:53 +0100

    python

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """A function that returns a turple"""
 
+from typing import Union
 from typing import Tuple
 
 
-def to_kv(k: str, v: float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A function for returning a tuple"""
     return (k, v**2)
