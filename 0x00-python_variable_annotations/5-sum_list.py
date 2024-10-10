553ddc53c2e88f04ba1dd8c3dca56e14ed667db5
chika-12 <markworship001@gmail.com>
2024-10-10 17:14:40 +0100

    python

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """A function to add up the list"""
 
+from typing import List
+
 
 def sum_list(input_list: float) -> float:
     """A function that returns the sum of a list of floats"""
-    result = 0.0
-    for num in input_list:
-        result += num
-    return result
+    return (sum(input_list))
