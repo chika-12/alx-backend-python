9f954db0bb897302ba5a2a31b27ff9443b1aace4
chika-12 <markworship001@gmail.com>
2024-10-15 17:09:59 +0100

    python

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,13 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """A function that loops 10 times"""
 
     for _ in range(10):
         await asyncio.sleep(1)
-        delay = random.uniform(0, 10)
+        delay = random.random() * 10
         yield delay
