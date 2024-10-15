deb6f3452231c28a603a6385a08c6b1c12a578d8
chika-12 <markworship001@gmail.com>
2024-10-15 17:49:26 +0100

    python

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""A function to generate random numbers"""
+
+import random
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """A function to generate"""
+
+    list_generated = []
+    list_generated = [number async for number in (async_generator())]
+    return list_generated
