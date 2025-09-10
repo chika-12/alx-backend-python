2e1536cd5282753555771649b18f710c32a0f4a7
chika-12 <markworship001@gmail.com>
2025-09-10 05:45:47 +0100

    task0

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,7 +1,11 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
 def log_queries(func):
   def logger(*args, **kwargs):
     query = ''
-    print("fetch users while logging the query")
     if args:
       query = args[0]
     else:
@@ -10,5 +14,16 @@ def log_queries(func):
     result = func(*args, **kwargs)
     return result
   return logger
-      
 
+@log_queries
+def fetch_all_users(query):
+  conn = sqlite3.connect('users.db')
+  cursor = conn.cursor()
+  cursor.execute(query)
+  results = cursor.fetchall()
+  conn.close()
+  return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+      
