da7663d5fd121a9ff62c015f5455476e6762c47f
chika-12 <markworship001@gmail.com>
2025-09-11 08:34:44 +0100

    enabled colum name display

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -2,12 +2,14 @@ import sqlite3
 
 def stream_users():
     conn = sqlite3.connect('ALX_prodev')
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
+
 
     cur = cursor.execute("SELECT * FROM user_data")
     try:
         for row in cur:
-            yield row
+            yield dict(row)
     finally:
         cur.close()
         conn.close()
