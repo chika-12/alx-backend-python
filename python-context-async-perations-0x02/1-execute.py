e48817d6958106e91c57e5b5bfdf668af6d61b7c
chika-12 <markworship001@gmail.com>
2025-09-11 23:48:43 +0100

    context-manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,25 +1,33 @@
+#!/usr/bin/python3
+
 import sqlite3
-from contextlib import contextmanager
+# from contextlib import contextmanager
 
 database_name = "../python-generators-0x00/ALX_prodev"
 
-@contextmanager
-def database(db_name, query):
-    conn = sqlite3.connect(db_name)
-    conn.row_factory = sqlite3.Row
-    cursor = conn.cursor()
+class Database:
+    def __init__(self, db_name, query):
+        self.db_name = db_name
+        self.conn = None
+        self.cursor = None
+        self.query = query
 
-    try:
-        cursor.execute(query)
-        yield cursor.fetchall()
-        conn.commit()
-    except: 
-        conn.rollback()
-    finally:
-        conn.close()
-with database(database_name, "SELECT * FROM users;") as data:
-    for val in data:
-        print(dict(val))
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query)
+        return self.cursor.fetchall()
 
+    def __exit__(self, exc_type, exc_val, exc_db):
+        if exc_type is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
+
+        self.conn.close()
+with Database(database_name, "SELECT * FROM users;") as data:
+    for row in data:
+        print(dict(row))
 
 
