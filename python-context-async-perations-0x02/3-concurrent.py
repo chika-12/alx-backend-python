c90828fb03d47f3395cd7923d4d59d362126c5a2
chika-12 <markworship001@gmail.com>
2025-09-12 04:10:07 +0100

    data

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -3,22 +3,22 @@ import aiosqlite
 
 db_file = "../python-generators-0x00/ALX_prodev"
 
-async def async_fetch_users(query):
+async def async_fetch_users():
     async with aiosqlite.connect(db_file) as db:
         db.row_factory = aiosqlite.Row
-        async with db.execute(query) as cursor:
+        async with db.execute("SELECT * FROM user_data;") as cursor:
             return await cursor.fetchall()
 
-async def async_fetch_older_users(query):
+async def async_fetch_older_users():
     async with aiosqlite.connect(db_file) as db:
         db.row_factory = aiosqlite.Row
-        async with db.execute(query) as cursor:
+        async with db.execute("SELECT * FROM user_data WHERE age > 50;") as cursor:
             return await cursor.fetchall()
 
 async def fetch_concurrently():
     users, older_users = await asyncio.gather(
-            async_fetch_users("SELECT * FROM user_data;"),
-            async_fetch_older_users("SELECT * FROM user_data WHERE age > 50;")
+            async_fetch_users(),
+            async_fetch_older_users()
             )
     print("All users", [dict(row) for row in users])
     print("Older users", [dict(row) for row in older_users])
