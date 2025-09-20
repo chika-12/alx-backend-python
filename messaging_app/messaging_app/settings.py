b268be0fd46f9209131a99d5593fe7d215e536f2
chika-12 <markworship001@gmail.com>
2025-09-20 23:25:54 +0100

    messging app configuration

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -121,3 +121,17 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    # Default permission: only authenticated users can access APIs
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    
+    # Default authentication methods
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication", 
+        "rest_framework.authentication.BasicAuthentication", 
+    ],
+}
