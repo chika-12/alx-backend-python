c22d34a19e9212b459341ed805d365dafde5b6eb
chika-12 <markworship001@gmail.com>
2025-09-21 00:23:02 +0100

    serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,16 +2,28 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    fields = ['user_id', 'first_name', 'last_name', 'email', 'role']
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'role']
 
 class MessageSerializer(serializers.ModelSerializer):
-  class Meta:
-    fields = ['message_id', 'sender', 'message_body', 'sent_at']
+    sender_name = serializers.SerializerMethodField()  # dynamic field
 
-class ConversationSerializers(serializers.ModelSerilizers):
-  participants = UserSerializer(many=True, read_only=True)
-  messages = MessageSerializer(many=True, read_only=True)  
-  class Meta:
-    fields = ['conversation_id', 'participants', 'messages', 'created_at']
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'sender_name', 'message_body', 'sent_at']
+
+    def get_sender_name(self, obj):
+        return f"{obj.sender.first_name} {obj.sender.last_name}"
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True)
+    messages = MessageSerializer(many=True, read_only=True)
+    total_messages = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 'total_messages', 'created_at']
+
+    def get_total_messages(self, obj):
+        return obj.messages.count()
