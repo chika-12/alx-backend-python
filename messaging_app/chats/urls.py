dbe11a10530e3ab7b713bfa75de2ce63814bdde5
chika-12 <markworship001@gmail.com>
2025-09-21 01:14:42 +0100

    setting up views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import MessageViewSet, ConversationViewSet, UserViewSet
 
-routers = DefaultRouter()
-routers.register(r'messages', MessageViewSet, basename='message')
-routers.register(r'conversations', ConversationViewSet, basename='conversation')
-routers.register(r'users', UserViewSet, basename='user')
+router = routers.DefaultRouter()
+router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(routers.urls)),
+    path('', include(router.urls)),
 ]
