3ddeba775ad6848ecc2ee0f7aac84ebe91cfd04d
chika-12 <markworship001@gmail.com>
2025-09-26 01:08:48 +0100

    permission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -7,10 +7,14 @@ class IsParticipantOfConversation(permissions.BasePermission):
     or the sender/recipient of the message.
   """
   def has_object_permission(self, request, view, obj):
-    if request.user == obj.sender:
-      return True
-    elif request.user in obj.conversation.participants.all():
-      return True
+
+    if not request.user.is_authenticated:
+      return False
+    
+    if request.method in permissions.SAFE_METHODS:
+      return request.user in obj.conversation.participants.all()
+    elif request.method in ["PUT", "PATCH", "DELETE"]:
+      return request.user == obj.sender
     else:
       return False
   
