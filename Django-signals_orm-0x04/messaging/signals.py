aefc3ca69657d804371c82e1e50d764106eff394
chika-12 <markworship001@gmail.com>
2025-10-04 13:33:02 +0100

    signals

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,7 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from .models import Message, Notification, MessageHistory
+from chats.models import User
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, create, **kwargs):
@@ -14,7 +15,20 @@ def logg_message_edit(sender, instance, **kwargs):
     if old_message.content != instance.content:
       MessageHistory.objects.create(
         message=instance,
-        edited_by=instance.sender
+        edited_by=instance.sender,
         old_content=old_message.content
       )
       instance.edited=True
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+  """
+  Deletes all messages, notifications, and message histories 
+  linked to the deleted user.
+  """
+  Message.objects.filter(sender=instance).delete()
+  Message.objects.filter(receiver=instance).delete()
+  Notification.objects.filter(user=instance).delete()
+  MessageHistory.objects.filter(edited_by=instance).delete()
+
+  print(f"🗑️ All data for user '{instance}' has been cleaned up.")
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -2,11 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 from .serializers import MessageHistorySerializers
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .models import MessageHistory
+from chats.models import User
+from chats.serializers import UserSerializer
 
 class MessageHistoryViewsets(viewsets.ReadOnlyModelViewSet):
   serializer_class = MessageHistorySerializers
   def queryset(self):
     message_id = self.kwargs.get("id")
     return MessageHistory.objects.filter(id=message_id).order_by("-edited_at")
+  
+class deleteUser(viewsets.ModelViewSet):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+  permission_classes = [IsAuthenticated]
+
+  def destroy(self, request):
+    user = self.get_object()
+    if request.user != user:
+      return Response({"error": "You can not delete this account"}, status=status.HTTP_403_FORBIDDEN)
+    
+    user.soft_delete()
+    return Response({"error": "Your account has been successfully deactivated"}, status=status.HTTP_204_NO_CONTENT)
